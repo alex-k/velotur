34cd80c413aed5c51413497f2519c1e26378e9c9
Alexei Kochetov <alex@kochetov.com>
2015-12-13 23:36:48 +0100

    - Use composer for build project

--- www/public/bee_index.php
+++ www/public/bee_index.php
@@ -1,21 +1,18 @@
 <?php
 ob_start();
-require_once(dirname(__FILE__).'/../bee/libs/config.lib.php');
+chdir(dirname(__DIR__));
+require_once('vendor/phpbee/phpbee/libs/config.lib.php');
 $gs_node_id=1;
 $cfg=gs_config::get_instance();
 mlog('1');
 
-if (strpos($_SERVER['REQUEST_URI'],'/superadmin')===0) $init=new gs_init('superadmin');
-	else $init=new gs_init('auto');
-
+$init=new gs_init('auto');
 
 cfg_set('tpl_data_dir',array(
 	cfg('tpl_data_dir'),
 	realpath(cfg('root_dir').'../html'),
 	));
 
-//$init->init(LOAD_CORE | LOAD_STORAGE | LOAD_TEMPLATES | LOAD_EXTRAS);
-//$init->load_modules();
 $init->init(LOAD_CORE);
 $init->load_modules();
 include_once('../classes/base.php');
