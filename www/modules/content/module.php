a16e0a66e66291f6ad077916043176e9cdc5fa4d
Alexei Kochetov <alex@kochetov.com>
2015-12-17 16:31:13 +0100

    Module to display '/content' pages

---
+++ www/modules/content/module.php
@@ -0,0 +1,32 @@
+<?php
+class module_content extends gs_base_module implements gs_module
+{
+    function __construct()
+    {
+    }
+
+    function install()
+    {
+    }
+
+    static function get_handlers()
+    {
+        $data = array(
+            'get_post' => array(
+                '' => 'handler_content.content',
+            )
+        );
+
+        return self::add_subdir($data, dirname(__file__));
+    }
+}
+
+class handler_content extends gs_base_handler
+{
+    function content($ret) {
+        $tpl=gs_tpl::get_instance();
+        $tpl->addTemplateDir(cfg('document_root').DIRECTORY_SEPARATOR.'content');
+        $this->params['name']=$this->data['gspgid_va'][0];
+        return $this->show($ret);
+    }
+}
