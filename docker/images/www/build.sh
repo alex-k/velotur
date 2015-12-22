840485769968678df2b278cf38b054d2adc0a0b7
Alexei Kochetov <alex@kochetov.com>
2015-12-21 22:18:06 -0500

    docker www image

--- docker/images/www/build.sh
+++ docker/images/www/build.sh
@@ -3,28 +3,10 @@ set -e
 set -x
 
 
-VOLUME_HOME="/var/lib/mysql"
 VOLUME_WWW="/var/www/velotur.ru"
 
 echo "=> Downloading app ..."
 git clone https://github.com/alex-k/velotur.git --depth 1 --branch develop --single-branch $VOLUME_WWW
 
-echo "=> Installing MySQL ..."
-mysql_install_db > /dev/null 2>&1
-
-echo "=> Runing mysql ..."
-service mysql start
-echo '=> Populate initial db'; 
-mysqladmin  create activeinfo_newvt
-mysql -e "GRANT ALL ON activeinfo_newvt.* TO activeinfo_newvt@localhost IDENTIFIED BY 'vt123' WITH GRANT OPTION"
-mysql -u activeinfo_newvt -pvt123 activeinfo_newvt < $VOLUME_WWW/mysql/mysql_data.sql
-echo "=> Done!"  
-
-/create_mysql_admin_user.sh
-
 echo "=> Downloading composer ..."
 cd $VOLUME_WWW/www && php -r "readfile('https://getcomposer.org/installer');" | php
-echo "=> Runing composer install ..."
-cd $VOLUME_WWW/www && php composer.phar install
-echo "=> Exiting mysql ..."
-service mysql stop
--- docker/images/www/run.sh
+++ docker/images/www/run.sh
@@ -1,37 +1,12 @@
 #!/bin/bash
 
-VOLUME_HOME="/var/lib/mysql"
 VOLUME_WWW="/var/www/velotur.ru"
 
-sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
-    -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php5/apache2/php.ini
+echo "=> Runing composer install ..."
+cd $VOLUME_WWW/www && php composer.phar install
 
-if [[ ! -d $VOLUME_HOME/mysql ]]; then
-    echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
-#    echo "=> Installing MySQL ..."
-#    mysql_install_db > /dev/null 2>&1
-#    echo "=> Downloading db structure ..."
-#    git clone https://github.com/alex-k/velotur.git --depth 1 --branch develop --single-branch /tmp/velotur.ru/
-#    echo '=> Populate initial db'; 
-#    /init_db.sh 
-#    echo "=> Done!"  
-#    /create_mysql_admin_user.sh
-else
-    echo "=> Using an existing volume of MySQL"
-fi
+source /etc/apache2/envvars
+tail -F /var/log/apache2/* &
+exec apache2 -D FOREGROUND
 
-if [[ ! -d $VOLUME_WWW ]]; then
-    echo "=> An empty application directory is detected in $VOLUME_WWW"
-#    echo "=> Runing mysql ..."
-#    service mysql start
-#    echo "=> Downloading app ..."
-#    git clone https://github.com/alex-k/velotur.git --depth 1 --branch develop --single-branch $VOLUME_WWW
-#    echo "=> Downloading composer ..."
-#    cd /var/www/velotur.ru/www && php -r "readfile('https://getcomposer.org/installer');" | php
-#    echo "=> Runing composer install ..."
-#    cd /var/www/velotur.ru/www && php composer.phar install
-#    echo "=> Exiting mysql ..."
-#    service mysql stop
-fi
-
-exec supervisord -n
+#exec supervisord -n
