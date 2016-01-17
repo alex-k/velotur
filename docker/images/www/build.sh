f908c239a3082b8fa75cc4251551f76f761d7423
Alexei Kochetov <alex@kochetov.com>
2016-01-17 21:25:49 +0100

    use VT_BRANCH in build step for www image

--- docker/images/www/build.sh
+++ docker/images/www/build.sh
@@ -2,11 +2,17 @@
 set -e
 set -x
 
+BRANCH=$1
+
+if [ -z "$BRANCH"];
+then
+  BRANCH='abcdefg'
+fi
 
 VOLUME_WWW="/var/www/velotur.ru"
 
-echo "=> Downloading app ..."
-git clone https://github.com/alex-k/velotur.git --depth 1 --branch develop --single-branch $VOLUME_WWW
+echo "=> Cloning app [branch $BRANCH]..."
+git clone https://github.com/alex-k/velotur.git --depth 1 --branch $BRANCH --single-branch $VOLUME_WWW
 
 echo "=> Downloading composer ..."
 cd $VOLUME_WWW/www && php -r "readfile('https://getcomposer.org/installer');" | php
