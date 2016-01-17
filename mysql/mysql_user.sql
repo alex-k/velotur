668c399c4f3139bc18a68dd383db591a1d388994
Alexei Kochetov <alex@kochetov.com>
2016-01-17 21:26:16 +0100

    SUPER mysql privilege for active info user

--- mysql/mysql_user.sql
+++ mysql/mysql_user.sql
@@ -1,2 +1,3 @@
 create database if not exists activeinfo_newvt;
 grant all privileges on activeinfo_newvt.* to activeinfo_newvt@'%' identified by 'vt123' with grant option;
+grant super on *.* to activeinfo_newvt@'%' identified by 'vt123' with grant option;
