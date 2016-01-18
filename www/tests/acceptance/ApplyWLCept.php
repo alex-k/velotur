e89b20c98c9d0b02ee7247a2c884cd25af92fc65
Alexei Kochetov <alex@kochetov.com>
2016-01-19 00:59:38 +0100

    Tour changed to one with no previous registration from the test user

--- www/tests/acceptance/ApplyWLCept.php
+++ www/tests/acceptance/ApplyWLCept.php
@@ -1,9 +1,9 @@
 <?php
 $I = new AcceptanceTester($scenario);
-$I->wantTo('Apply on the WL for tour 310');
+$I->wantTo('Apply on the WL for tour 304');
 
 
-$I->amOnPage('/apply.php?tourID=310');
+$I->amOnPage('/apply.php?tourID=304');
 
 
 $I->fillField('#formLoginEmail','alex@kochetov.com');
@@ -14,8 +14,8 @@ $I->click('#formLoginSubmit');
 
 $I->canSee('Вы собираетесь подать заявку');
 $I->canSee('в СПИСОК ОЖИДАНИЯ');
-$I->canSee('Лыжи: По монастырям Русского севера');
-$I->canSee('05.01.2016-09.01.2016');
+$I->canSee('Замки Мозеля и Рейна');
+$I->canSee('28.05.2016-05.06.2016');
 
 $I->seeElement(['name'=>'formSubmit']);
 $I->seeInSource('<input type=submit value="Подтверждаю" name=formSubmit class=input>');
