e3055ba75eb39c39994726f9694783bfd262f00d
vlad <vssmirnov@gmail.com>
2017-11-22 16:44:05 -0500

    Add babel react preset

--- www-react/webpack.config.js
+++ www-react/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         test: /\.js$/,
         loader: "babel-loader",
         options: {
-          presets: ["babel-preset-env"]
+          presets: ["babel-preset-env", "babel-preset-react"]
         }
       }
     ]
