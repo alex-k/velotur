7d94a9c72a3bf3b32c7bf67bd8a741cec0b33638
vlad <vssmirnov@gmail.com>
2017-11-22 18:34:00 -0500

    Add react-root and top-component

--- www-react/src/js/index.js
+++ www-react/src/js/index.js
@@ -1,1 +1,5 @@
-console.log("hello");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TopComponent from 'components/top-component';
+
+ReactDOM.render(<TopComponent />, document.getElementById('react-root'));
---
+++ www-react/src/js/react-components/top-component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TopComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1> Velotur </h1>
+  }
+}
+
+export default TopComponent
